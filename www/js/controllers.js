b2405f79571b9fd5608f12be894779779e9c7b6a
chris-anderson67 <chrisja67@gmail.com>
2016-10-16 18:08:00 -0400

    dropdown list working, populated

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,20 +11,50 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  Events.getPromise().success(function(response) {
-    $scope.events = response;
-  }).then(function (data) {
-    Events.store(data);
+  // Events.getPromise().success(function(response) {
+  //   $scope.events = response;
+  // }).then(function (data) {
+  //   Events.store(data);
+  // });
+  Events.get(function(data) {
+      $scope.events = data;
+      console.log('$scope.events: %o', $scope.events);    
   });
+  $scope.groups = [];
+  for (var i=0; i<10; i++) {
+    $scope.groups[i] = {
+      name: i,
+      items: []
+    };
+    for (var j=0; j<3; j++) {
+      $scope.groups[i].items.push(i + '-' + j);
+    }
+  }
+  
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    if ($scope.shownGroup === group && $scope.shownGroup.subevents) {
+	return true;
+    }
+  };
 
 
-  Events.store($scope.events);
 }])
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Events) {
-  $scope.events = Events.events;
-  console.log("CHDC");
-  console.log($scope.events);
+  console.log($stateParams.eventId);
+  $scope.event = Events.getEvent($stateParams.eventId);
+  console.log($scope.event);
 })
 
 .controller('AccountCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,18 +1,38 @@
 angular.module('starter.services', [])
 
 .factory('Events', ['$http', function($http) {
-  var events;
+  // var events;
 
-  var getEvents = function() {
-    return $http.get('/test2.json').success(function(eventData) {}); 
-  };
-  var storeEvents = function(events_array) {
-   events = events_array; 
-   console.log("Storing: ", events);
+  // var getEvents = function() {
+  //   return $http.get('/test2.json').success(function(eventData) {}); 
+  // };
+  // var storeEvents = function(events_array) {
+  //  events = events_array; 
+  //  console.log("Storing: ", events);
+  // };
+
+  var data;
+  return {
+    get: function (callback) {
+      if (data) {
+	callback(data);
+      } else {
+	console.log("HTTP_RQ");
+	$http.get('/test2.json').success(function(d) {
+	  callback(data = d);
+	});
+      }
+    },
+    getEvent: function(id) {
+      if (data) {
+        console.log("GETEVENT");
+        return data[id - 1];
+      }
+    }
   };
 
-  return { 
-    getPromise: getEvents,
-    store: storeEvents
-  };
+  // return { 
+  //   getPromise: getEvents,
+  //   store: storeEvents
+  // };
 }]);
