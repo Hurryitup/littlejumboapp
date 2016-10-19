9741e11c1c4648491c6a452b8690dd9b7313ce79
Daniel Navon <dnavon2557@gmail.com>
2016-10-19 18:56:45 -0400

    added endpoint to send schemas

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ var db = MongoClient.connect(mongoUri, function(error, databaseConenction) {
 });
 mongoose.connect(mongoUri);
 
+var Schema = mongoose.Schema;
+var VirtualType = mongoose.VirtualType;
 var StdEvent = require('./models/std_event');
 var CompEvent = require('./models/comp_event');
 var VDay = require('./models/visiting_day');
@@ -29,6 +31,47 @@ var VDay = require('./models/visiting_day');
 app.get('/', function (req, res) {
     res.render("index",{title: "Hey", message: "test"});
 });
+
+/*should be abstracted to a module but I will leave in here for now*/
+
+/* Description: Function flattenSchema takes in an Object that is in the format
+ * of a Schema.tree and returns the object with fields as strings or nested
+ * flattened schemas. Works recursively
+ */
+function flattenSchema( schema) {
+    for (var key in schema) {
+        if (key[0] == '_') {
+            delete schema[key]
+        } else if (schema[key].type) {
+            if (schema[key].type instanceof Schema) {
+                schema[key].type = flattenSchema(schema[key].type.tree);
+            } else if (schema[key].type.name) {
+                schema[key].type = schema[key].type.name;
+            } else if (schema[key].type instanceof VirtualType) {
+                delete schema[key];
+            }
+        } else if (schema[key].name) {
+            schema[key] = schema[key].name;
+        }  else if (schema[key] instanceof VirtualType) {
+                delete schema[key];
+        }
+                
+    }
+    return schema;
+}
+
+
+
+app.get('/getSchema', function (req, res) {
+    var Model  = mongoose.model(req.query.model);
+//    console.log(Model.schema.tree);
+    schemaTree = Model.schema.tree;
+    schemaTree = flattenSchema(schemaTree);
+    console.log(schemaTree);
+    return res.send(schemaTree);
+});
+
+
 
 app.get('/listVdays', function (req, res) {
     VDay.find().lean().exec(function (err, days) {
@@ -51,20 +94,5 @@ app.get('/vday', function (req, res) {
 });
 
 
-/*Sample requests (not functional or good)
-app.get('/update',  function (req, res) {
-    var updatepage = Handlebars.compile(eventpage)
-    updatepage = updatepage(eventSchema)
-    res.send(updatepage);
-       var collname = req.query.collname;
-       var newCollection = mongoose.model(collname, eventSchema);
-       res.send(null)
-}
-
-app.get('listStudentEventsOnDate', function (req, res) {
-    var collname = req.query.date
-    newCollection = mongoose.model(collname);
-
-*/
 app.listen(process.env.PORT || 5000);
 
