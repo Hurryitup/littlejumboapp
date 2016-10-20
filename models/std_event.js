5fae15ac3e4cb5348d50b939c800f325d78c3324
Daniel Navon <dnavon2557@gmail.com>
2016-10-19 20:23:53 -0400

    fixed location in standalone event

--- models/std_event.js
+++ models/std_event.js
@@ -9,17 +9,20 @@ var lastUpdatedSchema = new Schema({
     time: {type: Date, required: true},
     author: {type: String, required: true}
 });
+var locationSchema = new Schema({
+    name: {type: String, required: true},
+    address: {type: String, required: true}
+});
 
 var stdEventSchema = new Schema({
     title: {type: String, required: true},
     coordinator: {type: String, required: true},
-    location: {name: String, address: String},
+    location: {type: locationSchema, required: true},
     std_or_prnt: {type: String, enum: ['Student', 'Parent']},
     balloon_color: String,
     description: {type: String, required: true},
     time: {type: timeSchema, required: true},
     last_updated: {type: lastUpdatedSchema, required: true}
-//    belongs_to_composite: Boolean
 });
 
 
