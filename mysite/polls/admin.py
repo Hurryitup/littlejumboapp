82a32b9be9e787f21ac2de6cc8ccbf07131ef33b
Daniel Navon <dnavon2557@gmail.com>
2017-02-26 19:47:31 -0500

    django app for back office entry created

---
+++ mysite/polls/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import VisitingDay, StandaloneEvent, CompositeEvent, SubEvent
+import nested_admin
+# Register your models here.
+
+class StandaloneEventInline(nested_admin.NestedStackedInline):
+    model = StandaloneEvent
+    extra = 1
+
+class SubEventInline(nested_admin.NestedStackedInline):
+    model = SubEvent
+    extra = 1
+
+class CompositeEventAdmin(nested_admin.NestedModelAdmin):
+    inlines = [SubEventInline]
+
+class CompositeEventInline(nested_admin.NestedStackedInline):
+    model = CompositeEvent
+    inlines = [SubEventInline]
+    extra = 1
+
+
+class VisitingDayAdmin(nested_admin.NestedModelAdmin):
+    inlines = [StandaloneEventInline, CompositeEventInline]
+
+admin.site.register(VisitingDay, VisitingDayAdmin)
+admin.site.register(StandaloneEvent)
+admin.site.register(CompositeEvent, CompositeEventAdmin)
---
+++ mysite/polls/models.py
@@ -0,0 +1,89 @@
+from __future__ import unicode_literals
+from colorful.fields import RGBColorField
+from django.db import models
+from django.utils import timezone
+from location_field.models.plain import PlainLocationField
+
+
+from django.db import models
+
+
+class VisitingDay(models.Model):
+    name = models.CharField(max_length=25)
+    date = models.DateTimeField('date of event')
+    def __str__(self):
+        return self.name
+class Event(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length = 255)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    class Meta : 
+        abstract = True
+    def __str__(self):
+        return self.name
+
+class SingleEvent(Event):
+    coordinator = models.CharField(max_length = 31)
+    STUDENT = 'S'
+    PARENT = 'P'
+    AUDIENCE_TYPE = (
+        (STUDENT, 'Student'),
+        (PARENT, 'Parent'),
+    )
+    audience = models.CharField(
+        max_length=1,
+        choices = AUDIENCE_TYPE,
+        blank = True,
+    )
+    RED = 'RED'
+    ORANGE = 'ORA'
+    YELLOW = 'YEL'
+    BLUE = 'BLU'
+    GREEN = 'GRE'
+    PURPLE = 'PUR'
+    BLACK = 'BLA'
+    WHITE = 'WHI'
+    COLORS = (
+        (RED, 'Red'),
+        (WHITE, 'White'),
+        (YELLOW, 'Yellow'),
+    )
+    """
+    balloon_color = models.CharField(
+        max_length = 1,
+        blank = True,
+        choices = COLORS
+        """
+    balloon_color = RGBColorField(blank=True, null=True)
+    address = models.CharField(max_length=255) 
+    location = PlainLocationField(based_fields=['address'], zoom=7)
+    """
+    visiting_day = models.ForeignKey(
+        VisitingDay, 
+        on_delete=models.CASCADE,
+    )
+    """
+    class Meta:
+        abstract = True
+
+
+class StandaloneEvent(SingleEvent):
+    visiting_day = models.ForeignKey(
+        VisitingDay,
+        on_delete = models.CASCADE,
+    )
+
+class CompositeEvent (Event):
+    visiting_day = models.ForeignKey(
+        VisitingDay,
+        on_delete = models.CASCADE,
+    )
+    
+class SubEvent(SingleEvent):
+    composite_event = models.ForeignKey(
+        CompositeEvent,
+        on_delete = models.CASCADE,
+    )
+    
+
